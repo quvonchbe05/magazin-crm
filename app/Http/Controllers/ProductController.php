e9e6835786a76364e2575f152e46f12117a629a2
quvonchbe05 <quvonchbe05@gmail.com>
2022-08-17 09:08:22 +0500

    Searchni qilib qoydim

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ProductController extends Controller
 {
@@ -69,5 +70,13 @@ class ProductController extends Controller
         $product = Product::findOrFail($id);
         $product->delete();
         return "Deleted";
+    }
+
+    public function searchProduct(Request $request)
+    {
+        $products = DB::table('products')->where('name','like','%',$request->name,'%')->get();
+        return response()->json([
+            'products' => $products
+        ]);
     }
 }
